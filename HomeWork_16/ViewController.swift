120e476e8ac38237e834ad2e7236fdbf468b2e1d
AnnaSelyukova <asselyukova@gmail.com>
2025-06-19 17:18:11 +0300

    Initial Commit

---
+++ HomeWork_16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork_16
+//
+//  Created by macbook on 19.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
